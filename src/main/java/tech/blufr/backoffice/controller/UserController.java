324790fa17af2b8782c43a601fd70670b96ccd26
Franklin Guimarães <30613803+GuimaraesFranklin@users.noreply.github.com>
2022-04-29 01:45:27 -0300

    dev:configuration for start application spring

--- src/main/java/tech/blufr/backoffice/controller/UserController.java
+++ src/main/java/tech/blufr/backoffice/controller/UserController.java
@@ -1,10 +1,10 @@
 package tech.blufr.backoffice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import tech.blufr.backoffice.domain.User;
 import tech.blufr.backoffice.util.DateUtil;
@@ -15,9 +15,9 @@ import java.util.List;
 @RestController
 @RequestMapping("user")
 @Log4j2
+@RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
 
     @GetMapping(path = "list")
     public List<User> list() {
